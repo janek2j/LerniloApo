7573d96303358eb9a15bc4708173242d0ff44b25
janek2j <janek2j@gmail.com>
2022-11-24 23:06:24 +0100

    add: classes AppUser, Deck, Card

---
+++ LerniloApo/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerniloApo
+{
+    internal class AppUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Deck> Decks { get; set; }
+    }
+}
---
+++ LerniloApo/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerniloApo
+{
+    internal class Card
+    {
+        public int Id { get; set; }
+        public string Front { get; set; }
+        public string Back { get; set; }
+        public string Extra { get; set; }
+        public string Image { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
