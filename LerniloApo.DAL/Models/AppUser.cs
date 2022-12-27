c1d2224db57b7f6c913fc594953c179ba1e903f9
janek2j <janek2j@gmail.com>
2022-12-27 09:09:34 +0100

    refactor 1 commit message amended: refactor 1b

--- LerniloApo.DAL/Models/AppUser.cs
+++ LerniloApo.DAL/Models/AppUser.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LerniloApo
+﻿namespace LerniloApo
 {
-    internal class AppUser
+    public sealed class AppUser
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public DateTime CreatedDate { get; set; }
-        public List<Deck> Decks { get; set; }
+        public List<Deck> Decks { get; set; } = new();
     }
 }
